188de1a1d6a7a86af25bf9f8de575afa04f0c787
Marcos Alberto  Queiroz <softmaq@gmail.com>
2021-08-09 23:05:12 -0300

    update user

---
+++ src/main/java/br/com/softmaq/SoftmaqApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.softmaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftmaqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmaqApiApplication.class, args);
+	}
+
+}
